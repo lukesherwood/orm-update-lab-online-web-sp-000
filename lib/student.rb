6102d108b493a0260a2a5479d17e47ea7cd61278
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-05 18:50:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,6 +51,10 @@ class Student
     student.save
   end
   
+  def self.new_from_db
+    
+  end
+  
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
