09204bf9283fa77bdaae640584fb789f808ecbf0
Luke Sherwood <luke.sherwood55@gmail.com>
2020-03-05 18:41:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,8 @@ def save
   VALUES (?, ?)
   SQL
   
-      DB[:conn].execute(sql, self.name, self.grade)
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 end
 
 end
